086e351c522002abd9a5ad8fee8f8dc818978a71
Resident <bionqine@gmail.com>
2023-08-15 11:02:08 +0200

    add: camera following the player

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private Transform _playerTransform;
+
+    private Vector3 _offset;
+
+    private void Start() => _offset = transform.position - _playerTransform.position;
+
+    private void FixedUpdate() => transform.position = _playerTransform.position + _offset;
+}
